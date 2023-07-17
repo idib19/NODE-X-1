999cf32c352bce4f9488705c4da0c30a8749ce3d
Idrissa <idiberthe19@gmail.com>
2023-07-17 13:27:39 -0400

    Premier push

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+
+export default function Home() {
+    return (
+        <div>
+            <h1> Hello administrateur</h1>
+
+            <Button> Click moi</Button>
+        </div>
+    )
+}
