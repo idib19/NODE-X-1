06eaee74ac3d150371443d3cd41077bde1045b58
Idrissa <idiberthe19@gmail.com>
2023-08-25 08:56:58 -0400

    Billboards Data Table configurated 50%

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cell-actions.tsx
@@ -0,0 +1,7 @@
+export const CellAction = () => {
+    return (
+        <div>
+            Action
+        </div>
+    )
+}
--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,13 +1,18 @@
 "use client"
+import { Plus } from "lucide-react"
+import { useParams, useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button"
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-import { Plus } from "lucide-react"
-import { useParams, useRouter } from "next/navigation";
+import { BillboardColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
 
 
-export const BillboardClient = () => {
+interface BillboardClientProps {
+    data: BillboardColumn[]
+}
+export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
 
     const router = useRouter();
     const params = useParams();
@@ -15,12 +20,14 @@ export const BillboardClient = () => {
     return (
         <>
             <div className="flex items-center justify-between">
-                <Heading title="BillBoard (0)" description="Manage billboards for your store" />
+                <Heading title={`BillBoards (${data.length})`} description="Manage billboards for your store" />
                 <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
                     <Plus className="mr-2 h-4 w-4" /> Add New
                 </Button>
             </div>
             <Separator />
+
+            <DataTable  searchKey="label"  columns={columns} data = {data}/>
         </>
 
     )
