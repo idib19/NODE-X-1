fa05624d0d0e96b147a6d8abeb8baa7c417c3a64
Idrissa <idiberthe19@gmail.com>
2024-01-15 08:02:36 -0500

    Creation of orderdetails page

---
+++ app/(dashboard)/store/[storeId]/(routes)/orders/components/cell-action.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import axios from "axios";
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+
+import { AlertModal } from "@/components/modals/alert-modals";
+import { Button } from "@/components/ui/button";
+import { 
+  DropdownMenu, 
+  DropdownMenuContent, 
+  DropdownMenuItem, 
+  DropdownMenuLabel, 
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+
+import { OrderColumn } from "./columns";
+
+interface CellActionProps {
+  data: OrderColumn;
+}
+
+export const CellAction: React.FC<CellActionProps> = ({
+  data,
+}) => {
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const params = useParams();
+
+  const onConfirm = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/${params.storeId}/orders/${data.id}`);
+      toast.success('Commande supprime.');
+      router.refresh();
+    } catch (error) {
+      toast.error('Une erreur est survenue');
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  const onCopy = (id: string) => {
+    navigator.clipboard.writeText(id);
+    toast.success('Id de commande copiée dans le presse papier.');
+  }
+
+  return (
+    <>
+      <AlertModal 
+        isOpen={open} 
+        onClose={() => setOpen(false)}
+        onConfirm={onConfirm}
+        loading={loading}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+
+          <DropdownMenuItem
+            onClick={() => onCopy(data.id)}
+          >
+            <Copy className="mr-2 h-4 w-4" /> Copier Id
+          </DropdownMenuItem>
+
+          <DropdownMenuItem
+            onClick={() => router.push(`/store/${params.storeId}/orders/${data.id}`)}
+          >
+            <Edit className="mr-2 h-4 w-4" /> Details
+          </DropdownMenuItem>
+
+          <DropdownMenuItem
+            onClick={() => setOpen(true)}
+          >
+            <Trash className="mr-2 h-4 w-4" /> Supprimer
+          </DropdownMenuItem>
+          
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
