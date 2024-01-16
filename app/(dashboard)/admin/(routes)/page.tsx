32567fd5c6f5b64b99c498c3f852fa4608262cb5
Idrissa <idiberthe19@gmail.com>
2024-01-15 22:02:23 -0500

    applying SRP : SINGLE RESPONSABILITY PRINCIPLE

--- app/(dashboard)/admin/(routes)/page.tsx
+++ app/(dashboard)/admin/(routes)/page.tsx
@@ -4,10 +4,10 @@ import { Separator } from "@/components/ui/separator";
 import { Overview } from "@/components/overview";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
-import { getTotalRevenue } from "@/actions/get-total-revenue";
+import { getTotalRevenue } from "@/actions/overview/get-total-revenue";
 import { getSalesCount } from "@/actions/get-sales-count";
-import { getGraphRevenue } from "@/actions/get-graph-revenue";
-import { getStockCount } from "@/actions/get-stock-count";
+import { getGraphRevenue } from "@/actions/overview/get-graph-revenue";
+import { getStockCount } from "@/actions/overview/get-stock-count";
 import { formatter } from "@/lib/utils";
 
 interface DashboardPageProps {
