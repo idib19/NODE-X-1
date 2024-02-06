d17bb4eacda10ac0fdf477e719354d35d2b367ba
Idrissa <idiberthe19@gmail.com>
2024-02-06 08:28:59 -0500

    Addind a vercel.json file for cors handling

--- app/api/[storeId]/checkoutEmail/route.ts
+++ app/api/[storeId]/checkoutEmail/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 import prismadb from "@/lib/prismadb";
 
 
-
 export async function POST(req: Request, { params }: { params: { storeId: string } }) {
     try {
         const { productIds, data } = await req.json();
